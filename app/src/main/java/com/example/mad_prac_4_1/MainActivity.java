3d51078ed4eda76cbe5827f90c614058ef7b4989
siddh <siddhmandirwala9297@gmail.com>
2023-09-13 10:43:47 +0530

    initial commit

--- app/src/main/java/com/example/mad_prac_4_1/MainActivity.java
+++ app/src/main/java/com/example/mad_prac_4_1/MainActivity.java
@@ -3,16 +3,27 @@ package com.example.mad_prac_4_1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callSecondActivity(view);
+            }
+        });
     }
+
 
     public void callSecondActivity(View view) {
         Intent i = new Intent(getApplicationContext(), SecondActivity.class);
@@ -20,3 +31,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
--- app/src/main/java/com/example/mad_prac_4_1/SecondActivity.java
+++ app/src/main/java/com/example/mad_prac_4_1/SecondActivity.java
@@ -1,17 +1,26 @@
 package com.example.mad_prac_4_1;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callFirstActivity(view);
+            }
+        });
     }
 
     public void callFirstActivity(View view) {
@@ -20,3 +29,5 @@ public class SecondActivity extends AppCompatActivity {
     }
 
 }
+
+
